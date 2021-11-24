41ef9097b279b979b421ed26ca41fc3310d0269b
son jin yeong <handsun000@gmail.com>
2021-11-24 13:49:57 +0900

    IpPort

---
+++ src/main/java/hello/typeconverter/type/IpPort.java
@@ -0,0 +1,14 @@
+package hello.typeconverter.type;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class IpPort {
+
+    private String ip;
+    private int port;
+}
